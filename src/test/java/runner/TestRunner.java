f9a69a69151e40801429cb0bba3442ba39b5524e
masum <56122183+masumrazait@users.noreply.github.com>
2024-11-05 01:33:21 +0530

    Added HTML Report and code change in TestRunner

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -3,6 +3,7 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/features", glue = "stepDefinition",monochrome = true)
+@CucumberOptions(features = "src/test/java/features", glue = "stepDefinition", monochrome = true,
+        plugin = {"html:target/cucumber-reports.html","json:target/cucumber-reports.json"})
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
