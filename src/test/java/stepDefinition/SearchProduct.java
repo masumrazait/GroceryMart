09585732dc15cecf5c0db72d8343203b5da661ba
masum <56122183+masumrazait@users.noreply.github.com>
2024-11-04 23:39:26 +0530

    code change

--- src/test/java/stepDefinition/SearchProduct.java
+++ src/test/java/stepDefinition/SearchProduct.java
@@ -1,12 +1,12 @@
 package stepDefinition;
 
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 import java.util.Iterator;
 import java.util.Set;
@@ -15,12 +15,12 @@ import java.util.concurrent.TimeUnit;
 public class SearchProduct {
 
     WebDriver driver;
-    String ProductName;
-    String ProductName2;
+    String landingPageProductName;
+    String offerPageProductName;
 
 
     @Given("user is on GreenCart Landing page")
-    public void user_is_on_green_cart_landing_page() throws InterruptedException {
+    public void user_is_on_green_cart_landing_page() {
         System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
@@ -33,8 +33,8 @@ public class SearchProduct {
     public void user_searched_with_shortname_and_extracted_actual_name_of_product(String ShortName) throws InterruptedException {
         driver.findElement(By.xpath("//input[@type='search']")).sendKeys(ShortName);
         Thread.sleep(3000);
-        ProductName = driver.findElement(By.cssSelector("h4.product-name")).getText().split("-")[0].trim();
-        System.out.println(ProductName + " is extracted from home page");
+        landingPageProductName = driver.findElement(By.cssSelector("h4.product-name")).getText().split("-")[0].trim();
+        System.out.println(landingPageProductName + " is extracted from home page");
     }
 
     @Then("user searched for {string} shortname in offers page")
@@ -42,17 +42,18 @@ public class SearchProduct {
         driver.findElement(By.linkText("Top Deals")).click();
         Set<String> s1 = driver.getWindowHandles();
         Iterator<String> i1 = s1.iterator();
-        String parentWindow = i1.next();
+        //String parentWindow = i1.next();
         String childWindow = i1.next();
         driver.switchTo().window(childWindow);
         driver.findElement(By.id("search-field")).sendKeys(ShortName);
-        ProductName2 = driver.findElement(By.xpath("//table[@class='table table-bordered']/tbody/tr[1]/td")).getText();
-        System.out.println(ProductName2 + " is extracted from offers page");
+        offerPageProductName = driver.findElement(By.xpath("//table[@class='table table-bordered']/tbody/tr[1]/td")).getText();
+        System.out.println(offerPageProductName + " is extracted from offers page");
 
     }
 
     @Then("validate product name in offers page match with landing page")
     public void validate_product_name_in_offers_page_match_with_landing_page() {
+        Assert.assertEquals(landingPageProductName, offerPageProductName);
         driver.quit();
     }
 }
