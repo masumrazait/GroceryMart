585b63bdf1be1281176968fa1048224c94ca38d0
masum <56122183+masumrazait@users.noreply.github.com>
2024-11-20 01:35:51 +0530

    Latest Code changes and everything working fine

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -3,7 +3,7 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/features", glue = "stepDefinition", monochrome = true,
+@CucumberOptions(features = "src/test/java/features", glue = "stepDefinition",
         plugin = {"html:Report/cucumber-reports.html","json:Report/cucumber-reports.json"})
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
