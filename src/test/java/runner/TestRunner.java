8babe6aed314936a02a544b4cf7c7db0b97c829d
masum <56122183+masumrazait@users.noreply.github.com>
2024-11-20 01:29:40 +0530

    code changes

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -4,6 +4,6 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/java/features", glue = "stepDefinition", monochrome = true,
-        plugin = {"html:target/cucumber-reports.html","json:target/cucumber-reports.json"})
+        plugin = {"html:Report/cucumber-reports.html","json:Report/cucumber-reports.json"})
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
