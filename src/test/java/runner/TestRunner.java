771956296fb737d12bf7b0b66d8776bbb9310166
masum raza <masumrazait@gmail.com>
2025-01-28 00:43:06 +0530

    Revert "code changes"
    This reverts commit 8b68282af6e83ebc317aa991fc537e508f7183ed.
    
    # Conflicts:
    #	Report/cucumber-reports.html
    #	Report/cucumber-reports.json
    #	src/test/java/features/searchProduct.feature
    #	src/test/java/pageObjects/PageObjectsManager.java
    #	target/classes/META-INF/maven/GroceryMart/GroceryMart/pom.properties
    #	target/test-classes/features/searchProduct.feature
    #	target/test-classes/pageObjects/PageObjectsManager.class
    #	test-output/Default suite/Default test.html
    #	test-output/Default suite/Default test.xml
    #	test-output/emailable-report.html
    #	test-output/index.html
    #	test-output/junitreports/TEST-runner.TestRunner.xml
    #	test-output/old/Default suite/methods-alphabetical.html
    #	test-output/old/Default suite/methods.html
    #	test-output/old/Default suite/toc.html
    #	test-output/old/index.html
    #	test-output/testng-results.xml

--- src/test/java/runner/TestRunner.java
+++
@@ -1,9 +0,0 @@
-package runner;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features = "src/test/java/features", glue = "stepDefinition",
-        plugin = {"html:Report/cucumber-reports.html","json:Report/cucumber-reports.json"})
-public class TestRunner extends AbstractTestNGCucumberTests {
-}
