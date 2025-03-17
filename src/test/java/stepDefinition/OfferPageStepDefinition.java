470162c31f34181ff68ad4612b180bdbf154d40e
masum raza <masumrazait@gmail.com>
2025-03-17 22:57:02 +0530

    code added

--- src/test/java/stepDefinition/OfferPageStepDefinition.java
+++ src/test/java/stepDefinition/OfferPageStepDefinition.java
@@ -11,22 +11,28 @@ import java.util.Set;
 public class OfferPageStepDefinition {
     TestContextSetup testContextSetup;
     public String offerPageProductName;
+
     public OfferPageStepDefinition(TestContextSetup testContextSetup) {
         this.testContextSetup = testContextSetup;
     }
 
     @Then("user searched for {string} shortname in offers page")
     public void user_searched_for_same_shortname_in_offers_page(String shortname) throws InterruptedException {
-        testContextSetup.driver.findElement(By.linkText("Top Deals")).click();
-        Set<String> s1 = testContextSetup.driver.getWindowHandles();
-        Iterator<String> i1 = s1.iterator();
-        String parentWindow = i1.next();
-        String childWindow = i1.next();
-        testContextSetup.driver.switchTo().window(childWindow);
+        switchToOffersPage();
         testContextSetup.driver.findElement(By.xpath("//input[@type='search']")).sendKeys(shortname);
         Thread.sleep(3000);
         offerPageProductName = testContextSetup.driver.findElement(By.xpath("//table[@class='table table-bordered']//tbody/tr[1]/td[1]")).getText();
         System.out.println(offerPageProductName + " is extracted from the Offers page");
+    }
+
+    public void switchToOffersPage() {
+        //if (testContextSetup.driver.getCurrentUrl().equalsIgnoreCase("https://rahulshettyacademy.com/seleniumPractise/#/offers"))
+        testContextSetup.driver.findElement(By.linkText("Top Deals")).click();
+        Set<String> windows = testContextSetup.driver.getWindowHandles();
+        Iterator<String> it = windows.iterator();
+        String parentWindow = it.next();
+        String childWindow = it.next();
+        testContextSetup.driver.switchTo().window(childWindow);
     }
 
     @Then("validate product name in offer page matches with Landing Page")
@@ -34,4 +40,6 @@ public class OfferPageStepDefinition {
         Assert.assertEquals(offerPageProductName, testContextSetup.landingPageProductName);
         testContextSetup.driver.quit();
     }
+
+
 }
